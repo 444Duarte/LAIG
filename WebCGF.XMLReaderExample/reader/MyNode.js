b4c5d6252072a17f22fc51083edcf5038d86e51e
tomislaaaav <m_botelho8@hotmail.com>
2015-10-06 18:49:02 +0100

    bug fix

--- WebCGF.XMLReaderExample/reader/MyNode.js
+++ WebCGF.XMLReaderExample/reader/MyNode.js
@@ -1,9 +1,10 @@
-function MyNode(scene, material, texture, descendants) {
+function MyNode(scene, material, texture, transformations, descendants) {
  	CGFobject.call(this,scene);
 	
 	this.material=material;
 	this.texture=texture;
 	this.descendants=descendants;
+	this.transformations=transformations;
  };
 
  MyNode.prototype = Object.create(CGFobject.prototype);
